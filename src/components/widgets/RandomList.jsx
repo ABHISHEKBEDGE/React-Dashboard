4477b6f84ccec4028fd92c7f2b3fc4d3c01472a5
ABHISHEKBEDGE <abhishekbedge1947@gmail.com>
2023-11-23 12:51:56 +0530

    first commit

---
+++ src/components/widgets/RandomList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const RandomList = () => {
+  return (
+    <div className='fbox randomlist'>
+      <h3>List</h3>
+      <ol>
+        <li>Abhishek Bedge</li>
+        <li>Amarnath Jagatap</li>
+        
+      </ol>
+    </div>
+  );
+};
+
+export default RandomList;
